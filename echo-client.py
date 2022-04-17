8df456e91a38ba4f818a76c785f502d3dafe166a
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-17 08:07:34 +0200

    entendiendo el código

--- echo-client.py
+++ echo-client.py
@@ -5,6 +5,9 @@ import socket
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
+#se crea una conexión capaz de mandar todo tipo de mensajes al servidor con el sendall
+#también se una la función connect para conectarse a un puerto y host
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b"Hello, world")
