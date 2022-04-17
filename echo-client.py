782726e2978d8ed8c36766038544099368b4631a
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-17 11:32:18 +0200

    pruebas de una app de chat  clienteservidor

--- echo-client.py
+++ echo-client.py
@@ -1,16 +1,25 @@
-# echo-client.py
-
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
 
-#se crea una conexión capaz de mandar todo tipo de mensajes al servidor con el sendall
-#también se una la función connect para conectarse a un puerto y host
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 34566  # socket server port number
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
 
-print(f"Received {data!r}")
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
--- echo-server.py
+++ echo-server.py
@@ -1,30 +1,31 @@
-# echo-server.py
-
-#primero siempre se tiene que improtar la librería y definir el host y el puerto
-
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-#se crea la conexión servidor con un socket en un bucle
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = 34566  # initiate port no above 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
 
-    #método bind, para asociar un socket con una red con un puerto y un host
-    s.bind((HOST, PORT))
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
 
-    #se escucha la conexión con el listen
-    s.listen()
+    conn.close()  # close the connection
 
-    #se aceta la conexión con el acept
-    conn, addr = s.accept()
 
-    #se crea un bucle capaz de leer cualquier mensaje mandado por un cliente
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+if __name__ == '__main__':
+    server_program()
