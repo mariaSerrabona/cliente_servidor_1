c7c86a2d524242ef40180cf9125acec7e374e1df
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-17 07:58:09 +0200

    tampoco me funciona

--- servidor.py
+++ servidor.py
@@ -7,7 +7,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Enlace de socket y puerto
 server_address = ('localhost', 10000)
 
-print >>sys.stderr, 'empezando a levantar %s puerto %s' % server_address
+#print(>>sys.stderr, 'empezando a levantar %s puerto %s' % server_address)
 
 #método bind para asociar un socket con un servidor
 sock.bind(server_address)
@@ -17,23 +17,25 @@ sock.listen(1)
 
 while True:
     # Esperando conexion
-    print >>sys.stderr, 'Esperando para conectarse'
+    #print >>sys.stderr, 'Esperando para conectarse'
 
     #accept, acepta una conexión
     connection, client_address = sock.accept()
 
     try:
-        print >>sys.stderr, 'concexion desde', client_address
+        #print >>sys.stderr, 'concexion desde', client_address
 
         # Recibe los datos en trozos y reetransmite
         while True:
             data = connection.recv(19)
-            print >>sys.stderr, 'recibido "%s"' % data
+            #print >>sys.stderr, 'recibido "%s"' % data
+
+
             if data:
-                print >>sys.stderr, 'enviando mensaje de vuelta al cliente'
+                #print >>sys.stderr, 'enviando mensaje de vuelta al cliente'
                 connection.sendall(data)
             else:
-                print >>sys.stderr, 'no hay mas datos', client_address
+                #print >>sys.stderr, 'no hay mas datos', client_address
                 break
 
     finally:
