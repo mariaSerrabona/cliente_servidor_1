bd31e8798b76c186e658ab9e5d15ea4018510773
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-17 09:51:46 +0200

    usando el código del campus

--- servidor.py
+++ servidor.py
@@ -1,43 +1,51 @@
+import selectors
+import types
 import socket
-import sys
-
-# Creando el socket TCP/IP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Enlace de socket y puerto
-server_address = ('localhost', 10000)
-
-#print(>>sys.stderr, 'empezando a levantar %s puerto %s' % server_address)
-
-#método bind para asociar un socket con un servidor
-sock.bind(server_address)
-
-# poner socket a modo de servidor
-sock.listen(1)
-
-while True:
-    # Esperando conexion
-    #print >>sys.stderr, 'Esperando para conectarse'
-
-    #accept, acepta una conexión
-    connection, client_address = sock.accept()
-
-    try:
-        #print >>sys.stderr, 'concexion desde', client_address
-
-        # Recibe los datos en trozos y reetransmite
-        while True:
-            data = connection.recv(19)
-            #print >>sys.stderr, 'recibido "%s"' % data
-
-
-            if data:
-                #print >>sys.stderr, 'enviando mensaje de vuelta al cliente'
-                connection.sendall(data)
-            else:
-                #print >>sys.stderr, 'no hay mas datos', client_address
-                break
-
-    finally:
-        # Cerrando conexion
-        connection.close()
+selector = selectors.DefaultSelector()
+def accept_conn(sock):
+conn, addr = sock.accept()
+print('Conexión aceptada en {}'.format(addr))
+# Ponemos el socket en modo de no-bloqueo
+conn.setblocking(False)
+data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+events = selectors.EVENT_READ | selectors.EVENT_WRITE
+selector.register(conn, events, data=data)
+def service_conn(key, mask):
+sock = key.fileobj
+data = key.data
+if mask & selectors.EVENT_READ:
+recv_data = sock.recv(BUFFER_SIZE)
+if recv_data:
+data.outb += recv_data
+else:
+print('Cerrando conexion en {}'.format(data.addr))
+selector.unregister(sock)
+sock.close()
+if mask & selectors.EVENT_WRITE:
+if data.outb:
+print('Echo desde {} a {}'.format(repr(data.outb), data.addr))
+sent = sock.send(data.outb)
+data.outb = data.outb[sent:]
+if __name__ == '__main__':
+host = socket.gethostname() # Esta función nos da el nombre de la máquina
+port = 12345
+BUFFER_SIZE = 1024 # Usamos un número pequeño para tener una respuesta rápida
+# Creamos un socket TCP
+socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Configuramos el socket en modo de no-bloqueo
+socket_tcp.setblocking(False)
+socket_tcp.bind((host, port))
+socket_tcp.listen()
+print('Socket abierto en {} {}'.format(host, port))
+socket_tcp.setblocking(False)
+# Registramos el socket para que sea monitoreado por las funciones selector,.select()
+selector.register(socket_tcp, selectors.EVENT_READ, data=None)
+while socket_tcp:
+events = selector.select(timeout=None)
+for key, mask in events:
+if key.data is None:
+accept_conn(key.fileobj)
+else:
+service_conn(key, mask)
+socket_tcp.close()
+print('Conexión terminada.')
