7f9920d6f8e00da0dcd14c725115438c7e54a83f
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-17 08:29:54 +0200

    funciónn2

--- multiconn_server.py
+++ multiconn_server.py
@@ -23,6 +23,26 @@ def accept_wrapper(sock):
 
 
 
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
+
 sel = selectors.DefaultSelector()
 
 
